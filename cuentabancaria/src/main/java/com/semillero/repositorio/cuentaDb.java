49b763990cdaea62dfd85abd402e8cb53bc9bbea
yojuancarlos <juancarlos.saldarriaga313@gmail.com>
2023-03-28 14:31:02 -0500

    crear tabala
    inserte el codigo para crear una tabla

--- cuentabancaria/src/main/java/com/semillero/repositorio/cuentaDb.java
+++ cuentabancaria/src/main/java/com/semillero/repositorio/cuentaDb.java
@@ -1,11 +1,39 @@
 package main.java.com.semillero.repositorio;
 
+import java.sql.DriverManager;
+
 public class cuentaDb implements repositoriocrud{
     
-    
-    
-    
-    
+    private String cadenaConexion;
+    public cuentaDb(){
+        try {
+            DriverManager.registerDriver(new org.sqlite.JDBC());
+            cadenaConexion="jdbc:sqlite:pruebas.db";
+            creartabla();
+        } catch (Exception e) {
+            System.err.println("Error de conexión con la base de datos: " + e);
+        }
+
+
+
+    }
+    public  void creartabla() {
+        try (Connection conexion = DriverManager.getConnection(cadenaconexion)) {
+            String sql="CREATE TABLE IF NOT EXISTS cuentas (\n"
+            + " id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+            + " propietario TEXT NOT NULL,\n"
+            + " saldo integer NOT NULL,\n"
+            + " tipo text NOT NULL ,\n"
+            + " sobrecargo integer NULL,\n"
+            + " abono integer  NULL,\n"
+            + " numerocuenta TEXT NOT NULL UNIQUE,\n"
+            + " numeroretiros integer  NULL,\n"
+            + ");";
+        } catch (Exception e) {
+            System.err.println("Error al crear la tabla: " + e);
+        }
+        
+    }
     
     
     
